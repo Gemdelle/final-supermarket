3c8a4740603cbe8d636360ca7305b8e505bfaf74
Gemdelle <milu200911@hotmail.com>
2023-11-16 00:31:36 -0300

    images.

--- game.py
+++ game.py
@@ -16,10 +16,18 @@ test_font = pygame.font.Font('font/Alkhemikal.ttf', 50)
 game_active = True
 running = True
 
-# Images
+# Bacgrkound & Store
 background = pygame.image.load('graphics/welcome.png').convert_alpha()
 background_rect = background.get_rect(center=(960, 120))
 
+plate = pygame.image.load('graphics/plate.png')
+plate_rect = plate.get_rect(center=(960, 600))
+
+# Food
+# breadturtle, 
+food_bread_turtle = pygame.image.load('graphics/food-breadturtle.png')
+food_bread_turtle = pygame.transform.scale(food_bread_turtle,(170,140))
+food_bread_turtle_rect = food_bread_turtle.get_rect(center=(960, 600))
 
 while running:  # The game will be continuously updated.
     for event in pygame.event.get():
@@ -32,6 +40,8 @@ while running:  # The game will be continuously updated.
 
     if game_active:
         screen.blit(background, background_rect)
+        screen.blit(plate, plate_rect)
+        screen.blit(food_bread_turtle, food_bread_turtle_rect)
 
     pygame.display.update() # update the screen
     clock.tick(60) # while True runs 60 times per second
